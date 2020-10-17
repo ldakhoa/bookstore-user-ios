f054f2359009585de421e09b32e80ae4a2c961aa
Khoa Le <ldakhoa1308@gmail.com>
2020-10-17 17:17:03 +0700

    Remove App Scene

---
+++ Classes/Models/Category.swift
@@ -0,0 +1,12 @@
+//
+//  Categories.swift
+//  bsuser
+//
+//  Created by Khoa Le on 17/10/2020.
+//
+
+import Foundation
+
+struct Categories {
+    let categoryName: String
+}
---
+++ Classes/Systems/AppSecurity.swift
@@ -0,0 +1,8 @@
+//
+//  AppSecurity.swift
+//  bsuser
+//
+//  Created by Khoa Le on 17/10/2020.
+//
+
+import Foundation
